528c6c8ab117eeea7366ee1696c73daf16234946
ayangd <jimmyhendrixx90@gmail.com>
2021-09-16 12:27:34 +0700

    Fix create user

--- pages/api/delete.ts
+++ pages/api/delete.ts
@@ -1,18 +1,19 @@
-import prisma from "../../lib/prisma";
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../lib/prisma';
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default async (req, res) => {
-	try {
-		const { id } = req.body;
-		if (!id) {
-			res.json({ error: "You should have an id!" });
-			return;
-		}
-		const user = await prisma.user.delete({
-			where: { id },
-		});
-		res.status(200).json(user);
-	} catch (error) {
-		res.status(400).json({ message: "Something went wrong!" });
-	}
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+        const { id } = req.body;
+        if (!id) {
+            res.json({ error: 'You should have an id!' });
+            return;
+        }
+        const user = await prisma.user.delete({
+            where: { id },
+        });
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).json({ message: 'Something went wrong!' });
+    }
 };
--- utils/fetcher.tsx
+++ utils/fetcher.tsx
@@ -1,11 +1,11 @@
-export const fetcher = (url, data) =>
-	fetch(window.location.origin + url, {
-		method: data ? "POST" : "GET",
-		credentials: "include",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(data),
-	}).then((r) => {
-		return r.json();
-	});
+export const fetcher = async function <D = any, R = any>(url: string, data: D) {
+    const response = await fetch(window.location.origin + url, {
+        method: data ? 'POST' : 'GET',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+    return (await response.json()) as R;
+};
